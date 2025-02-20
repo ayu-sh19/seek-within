e8d7b83a918d2d62fac1174a3b35c70d317b7413
ayu-sh19 <ayuysh.sh@gmail.com>
2025-02-21 01:19:03 +0530

    pdf renderer

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -30,7 +30,7 @@ export const ourFileRouter = {
           key: file.key,
           name: file.name,
           userId: metadata.userId,
-          url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+          url: file.ufsUrl,
           uploadStatus: "PROCESSING",
         },
       });

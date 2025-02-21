f9dfd1a6ac201499df551c492f0e6517965d4efb
ayu-sh19 <ayuysh.sh@gmail.com>
2025-02-21 12:23:37 +0530

    loading states for file to load chats

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -2,7 +2,7 @@ import { notFound, redirect } from "next/navigation";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { db } from "@/db";
 import PdfRenderer from "@/components/PdfRenderer";
-import ChatWrapper from "@/components/ChatWrapper";
+import ChatWrapper from "@/components/chat/ChatWrapper";
 
 interface PageProps {
   params: {
@@ -43,7 +43,7 @@ const Page = async ({ params }: PageProps) => {
 
         {/* Right Side */}
         <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
-          <ChatWrapper />
+          <ChatWrapper fileId={file.id} />
         </div>
       </div>
     </div>

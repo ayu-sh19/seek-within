496e7a8e240c1ebf70744393e452cc21eb696e04
ayu-sh19 <ayuysh.sh@gmail.com>
2025-02-25 00:37:19 +0530

    fixing handleAuth

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,8 +1,12 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextRequest } from "next/server";
+// import { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest, { params }: any) {
-  const locale = await params;
-  const endpoint = locale.kindeAuth;
-  return handleAuth(request, endpoint);
-}
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+// export async function GET(request: NextRequest, { params }) {
+//   const locale = await params;
+//   const endpoint = locale.kindeAuth;
+//   return handleAuth(request, endpoint);
+// }
+
+export const GET = handleAuth() as any;

ed17956144603ff80f5047142e3ad1bb48818b2a
ayu-sh19 <ayuysh.sh@gmail.com>
2025-02-25 00:52:24 +0530

    removed hardcoded trpc client url

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,12 +4,13 @@ import { QueryClient } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import { PropsWithChildren, useState } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { absoluteUrl } from "@/lib/utils";
 
 const Providers = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
-      links: [httpBatchLink({ url: "http://localhost:3000/api/trpc" })],
+      links: [httpBatchLink({ url: absoluteUrl("/api/trpc") })],
     })
   );
 
